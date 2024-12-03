a1bdb64b742d4bdd938aca093e04af1935c21e21
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-12-03 19:51:09 +0300

    Add files via upload

---
+++ module_0.py
@@ -0,0 +1,21 @@
+# "1st program"
+print((9**0.5) * 5)
+
+# "2nd program"
+if 9.99 > 9.98 and 1000 != 1001:
+    print(True)
+
+# "3rd program"
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+c = a == b
+print(a)
+print(b)
+print(c)
+
+# "4th program"
+stroke = '123.456'
+transform = float(stroke)
+change_number = transform * 10
+result = int(change_number) % 10
+print(result)
